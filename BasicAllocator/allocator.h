a485a8c7e71f89fb0ef08f8b992aab18e0f87323
ddeka0 <dekadebashish3@gmail.com>
2022-12-29 14:54:13 +0530

    Basic Allocator: Map is also working

--- BasicAllocator/allocator.h
+++ BasicAllocator/allocator.h
@@ -10,15 +10,32 @@ template <typename DataType>
 class Allocator {
  public:
   using value_type = DataType;
+  Allocator() = default;
+
+  template <class U>
+  constexpr Allocator(const Allocator<U>&) noexcept {
+    LOG(INFO);
+  }
+
   void deallocate(DataType* p, std::size_t n) {
     free(p);
     LOG(INFO) << "deallocation done. p = " << p << ", size = " << n;
+    report(p, n, 0);
   }
 
   DataType* allocate(std::size_t n) {
+    LOG(INFO) << "DataType Size = " << sizeof(DataType);
     DataType* p = (DataType*)malloc(n * sizeof(DataType));
     LOG(INFO) << "allocation done. p = " << p << ", size = " << n;
+    report(p, n);
     return p;
+  }
+
+ private:
+  void report(DataType* p, std::size_t n, bool alloc = true) const {
+    std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(DataType) * n
+              << " bytes at " << std::hex << std::showbase
+              << reinterpret_cast<void*>(p) << std::dec << '\n';
   }
 };
 
--- BasicAllocator/main.cpp
+++ BasicAllocator/main.cpp
@@ -1,4 +1,5 @@
 #include <glog/logging.h>
+#include <map>
 #include <vector>
 #include "allocator.h"
 
@@ -7,12 +8,28 @@ int main(int argc, char* argv[]) {
   // FLAGS_minloglevel = 2;
   google::InitGoogleLogging(argv[0]);
 
+  /************************************************/
   std::vector<int, BasicAllocator::Allocator<int>> v;
 
   v.push_back(10);
   v.push_back(11);
   v.push_back(12);
-  //v.push_back(13);
+  v.push_back(13);
+
+  v.pop_back();
+  std::vector<int> v4;
+  auto v2 = v;
+  auto v3 = move(v);
+  if (v2 == v3) {
+    LOG(INFO) << "v2 and v3 are equal vectors";
+  }
+  // v4 = v2;
+  /************************************************/
+  LOG(INFO) << "Map>";
+  std::map<int, int, std::less<int>,
+           BasicAllocator::Allocator<std::pair<const int, int>>>
+      M;
+  M[1] = 1;
 
   LOG(INFO) << "main end";
 }
